48fad948bac2156bde0f74310d2614aba0ce7ba2
axel-colm <axel.colmant@gmail.com>
2025-03-14 17:36:08 +0100

    evaluate mobility

--- chessai/evaluation.py
+++ chessai/evaluation.py
@@ -1,23 +1,57 @@
 import chess
 
 def evaluate_material(board: chess.Board, player: chess.Color) -> float:
+    """
+    Evaluate the material on the board for the given player.
+    Each piece is assigned a value:
+    - Pawn: 1
+    - Knight: 3
+    - Bishop: 3
+    - Rook: 5
+    - Queen: 9
+    - King: 200
+    The evaluation is the sum of the values of the player's pieces minus the sum of the values of the opponent's pieces.
+    The evaluation is -9999 if the player is checkmated, 9999 if the opponent is checkmated, and 0 if the game is a stalemate or if there is insufficient material.
+    
+    Args:
+        board (chess.Board): The board to evaluate.
+        player (chess.Color): The player for which to evaluate the material.
+        
+    """
     if board.is_checkmate():
-        if board.turn == player:
-            return -9999
-        else:
-            return 9999
+        return -9999 if board.turn == player else 9999
     elif board.is_stalemate() or board.is_insufficient_material():
         return 0
-
+    
     eval = 0
     values = {
         chess.PAWN: 1,
         chess.KNIGHT: 3,
         chess.BISHOP: 3,
         chess.ROOK: 5,
-        chess.QUEEN: 9
+        chess.QUEEN: 9,
+        chess.KING: 200
     }
     for piece_type, value in values.items():
         eval += value * len(board.pieces(piece_type, player))
         eval -= value * len(board.pieces(piece_type, not player))
+    return eval
+
+
+def evaluate_mobility(board: chess.Board, player: chess.Color) -> float:
+    """
+    Evaluate the mobility on the board for the given player.
+    The evaluation is the number of legal moves for the player minus the number of legal moves for the opponent.
+    
+    Args:
+        board (chess.Board): The board to evaluate.
+        player (chess.Color): The player for which to evaluate the mobility.
+        
+    """
+    board = board.copy()
+    eval = 0
+    for color in chess.COLORS:
+        board.turn = color
+        sign = 1 if color == player else -1
+        eval += sign * len(list(board.legal_moves))
     return eval
--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -3,10 +3,12 @@ sys.path.append("..")
 import chess
 import unittest
 from chessai.evaluation import (
-    evaluate_material
+    evaluate_material,
+    evaluate_mobility
 )
 
 def log_board_and_scores(board, wscore, bscore, eval_func_name):
+    print("---")
     print(board.unicode(empty_square="."))
     print(f"{eval_func_name} - White score: {wscore}")
     print(f"{eval_func_name} - Black score: {bscore}")
@@ -74,13 +76,35 @@ class TestEvaluateMaterial(unittest.TestCase):
         self.assertEqual(wscore, -1)
         self.assertEqual(bscore, 1)
         
-    def test_stalemate(self):
-        board = chess.Board("8/8/8/8/8/6K1/7Q/7k w - - 0 1")
-        board.turn = chess.BLACK
+    def test_checkmate_white_wins(self):
+        """
+        Test the material evaluation when white checkmates black.
+        """
+        board = chess.Board("8/8/8/8/8/K7/1Q6/k7 b - - 0 1")
         wscore = evaluate_material(board, chess.WHITE)
         bscore = evaluate_material(board, chess.BLACK)
-        self.assertEqual(wscore, 9999)
-        self.assertEqual(bscore, -9999)
+        self.assertEqual(wscore, 9999, "White should have a score of 9999 when checkmating black")
+        self.assertEqual(bscore, -9999, "Black should have a score of -9999 when checkmated by white")
+
+    def test_checkmate_black_wins(self):
+        """
+        Test the material evaluation when black checkmates white.
+        """
+        board = chess.Board("K7/1q6/k7/8/8/8/8/8 w - - 0 1")
+        wscore = evaluate_material(board, chess.WHITE)
+        bscore = evaluate_material(board, chess.BLACK)
+        self.assertEqual(wscore, -9999, "White should have a score of -9999 when checkmated by black")
+        self.assertEqual(bscore, 9999, "Black should have a score of 9999 when checkmating white")
+
+    def test_stalemate(self):
+        """
+        Test the material evaluation when the game is a stalemate.
+        """
+        board = chess.Board("K7/8/1q6/k7/8/8/8/8 w - - 0 1")
+        wscore = evaluate_material(board, chess.WHITE)
+        bscore = evaluate_material(board, chess.BLACK)
+        self.assertEqual(wscore, 0, "Material score should be 0 in a stalemate")
+        self.assertEqual(bscore, 0, "Material score should be 0 in a stalemate")
         
     def test_insufficient_material(self):
         board = chess.Board("8/8/8/8/8/6B1/5K2/7k w - - 0 1")
@@ -89,13 +113,37 @@ class TestEvaluateMaterial(unittest.TestCase):
         self.assertEqual(wscore, 0)
         self.assertEqual(bscore, 0)
         
-    def test_pat(self):
-        board = chess.Board("8/8/8/8/8/7k/5q2/7K w - - 0 1")
-        board.turn = chess.WHITE
-        wscore = evaluate_material(board, chess.WHITE)
-        bscore = evaluate_material(board, chess.BLACK)
-        self.assertEqual(wscore, 0)
-        self.assertEqual(bscore, 0)        
 
+class TestEvaluateMobility(unittest.TestCase):
+    
+    def test_initial_board(self):
+        board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")    
+        wscore = evaluate_mobility(board, chess.WHITE)
+        bscore = evaluate_mobility(board, chess.BLACK)
+        self.assertEqual(wscore, 0)
+        self.assertEqual(bscore, 0)
+    
+    def test_white_advantage(self):
+        """
+        Test the mobility evaluation when white has a mobility advantage.
+        """
+        board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        board.push_san("e4")
+        wscore = evaluate_mobility(board, chess.WHITE)
+        bscore = evaluate_mobility(board, chess.BLACK)
+        self.assertGreater(wscore, 0, "White should have a positive mobility score")
+        self.assertLess(bscore, 0, "Black should have a negative mobility score")
+
+    def test_black_advantage(self):
+        """
+        Test the mobility evaluation when black has a mobility advantage.
+        """
+        board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1")
+        board.push_san("e5")
+        wscore = evaluate_mobility(board, chess.WHITE)
+        bscore = evaluate_mobility(board, chess.BLACK)
+        self.assertLess(wscore, 0, "White should have a negative mobility score")
+        self.assertGreater(bscore, 0, "Black should have a positive mobility score")
+        
 if __name__ == "__main__":
     unittest.main()
